0f5bdf3605d0a21aff476d2ecae926b4957946a0
Saurav <official.srv.modak@gmail.com>
2021-03-17 14:40:05 +0530

    bug fixes

--- app/src/main/java/com/sourav/shop/SplashScreen.java
+++ app/src/main/java/com/sourav/shop/SplashScreen.java
@@ -43,6 +43,6 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 3500);
+        }, 5000);
     }
 }
