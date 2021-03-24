0732fa0d717a236ad953e3c0352ab9dc371b0cdc
Saurav <official.srv.modak@gmail.com>
2021-03-24 19:08:36 +0530

    minor UI fixes

--- app/src/main/java/com/sourav/shop/OrderDetails.java
+++ app/src/main/java/com/sourav/shop/OrderDetails.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -65,6 +66,14 @@ public class OrderDetails extends AppCompatActivity {
                 Intent searchIntent = new Intent(OrderDetails.this, SearchActivity.class);
                 searchIntent.putExtra("user_data", userInfo.toString());
                 startActivity(searchIntent);
+            }
+        });
+
+        Button cancelBtn = findViewById(R.id.orderDetailsCancelBtn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
 
