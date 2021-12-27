10330901f77e0ac822a0d74e1b22557ed0eaad1c
souravmodak <official.srv.modak@gmail.com>
2021-12-27 15:23:33 +0530

    Updated build properties

--- app/src/main/java/com/sourav/kisara/ThirdPartyAuth.java
+++ app/src/main/java/com/sourav/kisara/ThirdPartyAuth.java
@@ -142,6 +142,7 @@ public class ThirdPartyAuth extends AppCompatActivity {
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            e.printStackTrace();
             Log.w("GOOGLE-SIGNIN-FAILED", "signInResult:failed code=" + e.getStatusCode());
             updateUIGoogleSignIn(null);
         }
